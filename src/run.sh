95b867649e1dd8126ac9654263a65c4ab411a230
ryan <ryanyt@hawaii.edu>
2017-11-29 17:33:59 -1000

    working

--- src/run.sh
+++ src/run.sh
@@ -5,12 +5,20 @@ HOSTFILE="./hostfile_64.txt"
 PLATFORM="./cluster_crossbar_64.xml"
 NP=64
 
+# Reduce Implementation
+S="smpi_binomial_reduce"
+B="binomial_reduce"
+G="greedy_reduce"
+
+VAR=$1
+REDUCE_TYPE=${!VAR}
+NUM_INTS=$2
+
 # Compile and Run
-smpicc -O3 reduce_skeleton.c greedy_reduce.c binomial_reduce.c -o reduce -lm \
+smpicc -O3 -DNUM_INTS=$NUM_INTS reduce_skeleton.c greedy_reduce.c binomial_reduce.c -o reduce -lm \
 && \
 smpirun --cfg=smpi/reduce:binomial \
-  --cfg=smpi/privatization:yes \
   -np $NP \
   -hostfile $HOSTFILE \
   -platform $PLATFORM \
-  ./reduce
+  ./reduce $REDUCE_TYPE
