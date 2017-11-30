0841ca6d3290eeafcad67cd2deb990325ea82610
ryan <ryanyt@hawaii.edu>
2017-11-29 22:51:06 -1000

    updated

--- src/reduce_skeleton.c
+++ src/reduce_skeleton.c
@@ -16,7 +16,7 @@ int main(int argc, char **argv) {
     local_sum[k] = 1;
   }
 
-  MPI_Init(&argc,&argv);
+  MPI_Init(&argc,&argv);  
   MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
   MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
 
