ccab94917bf3caee00a376375310a0d282cd7cea
ryan <ryanyt@hawaii.edu>
2017-11-29 17:33:46 -1000

    made to work with shell script

--- src/reduce_skeleton.c
+++ src/reduce_skeleton.c
@@ -2,16 +2,13 @@
 #include <stdio.h>
 #include <mpi.h>
 
-#define NUM_INTS 1000000
-
 int Reduce_greedy( void *, void *, int, MPI_Datatype, MPI_Op, int, MPI_Comm);
 int Reduce_binomial( void *, void *, int, MPI_Datatype, MPI_Op, int, MPI_Comm);
 
 int main(int argc, char **argv) {
-  // IMPORTANT: each process has its own i, my_rank, nprocs, x
   int i, my_rank, num_procs;
-  //int local_sum[5] = {1,2,3,4,5};
   int *local_sum, *global_sum;
+  char *reduce_implementation_name;
 
   local_sum = malloc(sizeof(int) * NUM_INTS);
   global_sum = malloc(sizeof(int) * NUM_INTS);
@@ -23,6 +20,8 @@ int main(int argc, char **argv) {
   MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
   MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
 
+  reduce_implementation_name = argv[1];
+
   // Start the timer
   double start_time;
   MPI_Barrier(MPI_COMM_WORLD);
@@ -30,14 +29,20 @@ int main(int argc, char **argv) {
     start_time = MPI_Wtime();
   }
 
-  //MPI_Reduce(local_sum, global_sum, NUM_INTS, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
-  //Reduce_binomial(local_sum, global_sum, NUM_INTS, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
-  Reduce_greedy(local_sum, global_sum, NUM_INTS, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+  if (strcmp(reduce_implementation_name, "smpi_binomial_reduce") == 0) {
+    MPI_Reduce(local_sum, global_sum, NUM_INTS, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+  }
+  else if (strcmp(reduce_implementation_name, "binomial_reduce") == 0) {
+    Reduce_binomial(local_sum, global_sum, NUM_INTS, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+  }
+  else if (strcmp(reduce_implementation_name, "greedy_reduce") == 0){
+    Reduce_greedy(local_sum, global_sum, NUM_INTS, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+  }
 
   // End timer and print result
   MPI_Barrier (MPI_COMM_WORLD);
   if (my_rank == 0) {
-    printf("REDUCE TIME: %.3lf seconds\n", MPI_Wtime() - start_time);
+    printf("%s | NUM_INTS: %d | %.3lf seconds\n", reduce_implementation_name, NUM_INTS, MPI_Wtime() - start_time);
 
     /*
     for (int j = 0; j < NUM_INTS; j++) {
