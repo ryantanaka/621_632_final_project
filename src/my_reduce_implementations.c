f4f28b6bb6c3010fa311d62794a35c29a0078aae
ryan <ryanyt@hawaii.edu>
2017-12-01 14:46:38 -1000

    pipeline seems to be working, need to test more

--- src/my_reduce_implementations.c
+++ src/my_reduce_implementations.c
@@ -1,5 +1,6 @@
 #include <mpi.h>
 #include <math.h>
+#include <stdio.h>
 
 // Num_ranks must be power of 2!
 int Reduce_binomial(void *sendbuf_notype, void* recvbuf_notype, int m,
@@ -51,6 +52,95 @@ MPI_Datatype mpi_datatype, MPI_Op mpi_op, int root, MPI_Comm mpi_comm) {
   }
 
   free(tempbuf);
+}
 
-  MPI_Barrier(mpi_comm);
+// segment size s must be (1 <= s <= m)
+int Reduce_pipeline(void *sendbuf_notype, void* recvbuf_notype, int s, int m,
+MPI_Datatype mpi_datatype, MPI_Op mpi_op, int root, MPI_Comm mpi_comm) {
+  int my_rank, num_ranks;
+  int i, j;
+  int s2, s3, snext, q;
+  int send_to, receive_from, start_rank;
+  MPI_Status status;
+
+  int *sendbuf, *recvbuf, *tempbuf;
+
+  sendbuf = (int *) sendbuf_notype;
+  recvbuf = (int *) recvbuf_notype;
+
+  MPI_Comm_rank(mpi_comm, &my_rank);
+  MPI_Comm_size(mpi_comm, &num_ranks);
+
+  send_to = (my_rank + num_ranks - 1) % num_ranks;
+  receive_from = (my_rank + num_ranks + 1) % num_ranks;
+  start_rank = (root + num_ranks - 1) % num_ranks;
+
+  tempbuf = (int*)malloc(s * sizeof(int));
+
+  if (my_rank == root) {
+    for (i = 0; i < m; i++) {
+      recvbuf[i] = sendbuf[i];
+    }
+  }
+  else {
+    for(i = 0; i < s; i++) {
+      tempbuf[i] = sendbuf[i];
+    }
+  }
+
+  q = m/s; // q is the number of chunks
+  if ((m % s) != 0) {
+    s2 = m % s;
+    q++;
+  }
+  else {
+    s2 = s;
+  }
+
+  // DEBUGGING
+  if (my_rank == root) {
+    printf("segment size s = %d, segment size 2 s2 = %d, num chunks q = %d\n", s, s2, q);
+  }
+
+  // im only sending chunks
+  if (my_rank == start_rank) {
+    for(i = 0; i < q; i++) {
+      s3 = (i == q - 1) ? s2 : s;
+
+      MPI_Send(tempbuf, s3, mpi_datatype, send_to, 0, mpi_comm);
+      if (i < q - 1) { //get the next chunk ready to send if there are chunks left to send
+        snext = (i == q - 2) ? s2 : s;
+        for(j = 0; j < snext; j++) {
+          tempbuf[j] = sendbuf[(i + 1) * s + j];
+        }
+      }
+    }
+  }
+  // im a rank in between start_rank and root
+  // receiving, reducing, then sending
+  else if (my_rank != root) {
+    for(i = 0; i < q; i++) {
+      s3 = (i == q - 1) ? s2 : s;
+
+      MPI_Recv(tempbuf, s3, mpi_datatype, receive_from, 0, mpi_comm, &status);
+      for (j = 0; j < s3; j++) {
+        tempbuf[j] += sendbuf[i * s + j];
+      }
+      MPI_Send(tempbuf, s3, mpi_datatype, send_to, 0, mpi_comm);
+    }
+  }
+  // im the root
+  // im only receiving then reducing chunks
+  else {
+    for(i = 0; i < q; i++) {
+      s3 = (i == q - 1) ? s2 : s;
+
+      MPI_Recv(tempbuf, s3, mpi_datatype, receive_from, 0 , mpi_comm, &status);
+      for(j = 0; j < s3; j++) {
+        recvbuf[i*s + j] += tempbuf[j];
+      }
+    }
+  }
+
+  free(tempbuf);
 }
