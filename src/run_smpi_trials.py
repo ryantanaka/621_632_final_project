4ce4562347a5a7fafc35f63322bc521b78f1fee9
ryan <ryanyt@hawaii.edu>
2017-12-02 02:37:51 -1000

    created for convenience

---
+++ src/run_smpi_trials.py
@@ -0,0 +1,9 @@
+import subprocess
+
+implementation = "G"
+run_script = "./run.sh {} {}"
+
+print("RUNNING IMPLEMENTATION: {}".format(implementation))
+
+for i in range(4, 24 + 1):
+    subprocess.Popen(run_script.format(implementation, 2**i), shell=True).wait()
