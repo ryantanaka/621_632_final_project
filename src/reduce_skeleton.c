b4c94d7788a2a0f001ca2fb6b0cf870ceb1012d6
ryan <ryanyt@hawaii.edu>
2017-12-06 15:12:13 -1000

    updated

--- src/reduce_skeleton.c
+++ src/reduce_skeleton.c
@@ -2,9 +2,10 @@
 #include <stdio.h>
 #include <mpi.h>
 
-int Reduce_greedy( void *, void *, int, MPI_Datatype, MPI_Op, int, MPI_Comm);
-int Reduce_binomial( void *, void *, int, MPI_Datatype, MPI_Op, int, MPI_Comm);
-int Reduce_pipeline( void *, void *, int, MPI_Datatype, MPI_Op, int, MPI_Comm);
+int Reduce_greedy(void *, void *, int, MPI_Datatype, MPI_Op, int, MPI_Comm);
+int Reduce_binomial(void *, void *, int, MPI_Datatype, MPI_Op, int, MPI_Comm);
+int Reduce_pipeline(void *, void *, int, MPI_Datatype, MPI_Op, int, MPI_Comm);
+int Reduce_binary(void *, void *, int, MPI_Datatype, MPI_Op, int, MPI_Comm);
 
 int main(int argc, char **argv) {
   int i, my_rank, num_procs;
@@ -14,7 +15,7 @@ int main(int argc, char **argv) {
   local_sum = malloc(sizeof(int) * NUM_INTS);
   global_sum = malloc(sizeof(int) * NUM_INTS);
   for (int k = 0; k < NUM_INTS; k++) {
-    local_sum[k] = 1;
+    local_sum[k] = k;
   }
 
   MPI_Init(&argc,&argv);
@@ -40,7 +41,7 @@ int main(int argc, char **argv) {
     Reduce_pipeline(local_sum, global_sum, NUM_INTS, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
   }
   else if (strcmp(reduce_implementation_name, "binary_pipeline_reduce") == 0) {
-    Reduce_binary(local_sum, global_sum, NUM_INTS, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+    Reduce_binary(local_sum, global_sum, NUM_INTS, MPI_INT, MPI_SUM, 2, MPI_COMM_WORLD);
   }
   else if (strcmp(reduce_implementation_name, "greedy_reduce") == 0){
     Reduce_greedy(local_sum, global_sum, NUM_INTS, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
@@ -48,16 +49,17 @@ int main(int argc, char **argv) {
 
   // End timer and print result
   MPI_Barrier (MPI_COMM_WORLD);
-  if (my_rank == 0) {
+  if (my_rank == 2) {
     printf("%s | NUM_INTS: %11d | %.10lf\n", reduce_implementation_name, NUM_INTS, MPI_Wtime() - start_time);
 
-    /*
     for (int j = 0; j < NUM_INTS; j++) {
       printf("%d ", global_sum[j]);
     }
     printf("\n");
-    */
   }
+
+  free(local_sum);
+  free(global_sum);
 
   MPI_Finalize();
 }
